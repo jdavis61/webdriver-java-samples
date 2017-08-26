75cdff761a240b69fc12a59a74df1ff6cd045555
James Davis <jrdavismaine@gmail.com>
2017-08-26 12:41:44 -0400

    Added validation test

--- src/test/java/com/maineqa/LoginTest.java
+++ src/test/java/com/maineqa/LoginTest.java
@@ -6,9 +6,8 @@ import com.maineqa.pages.SecuredPage;
 import com.maineqa.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class LoginTest {
 
@@ -19,7 +18,11 @@ public class LoginTest {
     public void setup() {
         this.driver = WebDriverFactory.createLocalChromeDriverInstance();
         this.navigationMenu = PageFactory.initElements(driver, NavigationMenu.class);
-        this.navigationMenu.loadNavigationMenu();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void loadNavigationMenu() {
+        navigationMenu.loadNavigationMenu();
     }
 
     @AfterClass(alwaysRun = true)
@@ -27,16 +30,49 @@ public class LoginTest {
         driver.quit();
     }
 
+    /*
     @Test(description = "Login Test")
-    public void loginTest() throws InterruptedException {
+    public void loginTest() {
         String username = "tomsmith";
         String password = "SuperSecretPassword!";
+        String subheading = "This is where you can log into the secure area. Enter tomsmith for the username and SuperSecretPassword! for the password. If the information is wrong you should see error messages.";
+
         FormAuthentication formAuthentication = navigationMenu.clickFormAuthentication();
+        Assert.assertEquals(formAuthentication.getFooterText(), "Powered by Elemental Selenium");
+        Assert.assertEquals(formAuthentication.getPageSubHeader(), subheading);
+        Assert.assertEquals(formAuthentication.getPageHeader(), "Login Page");
 
         SecuredPage securedPage = formAuthentication.login(username, password);
+        Assert.assertEquals(securedPage.getPageHeader(), "Secure Area");
+        Assert.assertEquals(securedPage.getFooterText(), "Powered by Elemental Selenium");
+    }
+**/
 
-        Thread.sleep(5000);
+    @Test(description = "Validation Message Test")
+    public void validationMessageTest() {
+        String username = "tomsmith";
+        String incorrectUsername = "timsmith";
+        String incorrectPassword = "qwerty100";
+        String usernameErrorMessage = "Your username is invalid!";
+        String passwordErrorMessage = "Your password is invalid!";
 
+        FormAuthentication formAuthentication = navigationMenu.clickFormAuthentication();
+
+        // enter incorrect username
+        formAuthentication.username.sendKeys(incorrectUsername);
+        formAuthentication.clickSubmitButton();
+        Assert.assertEquals(formAuthentication.getPageMessage(), usernameErrorMessage);
+
+        // enter incorrect password
+        formAuthentication.username.sendKeys(username);
+        formAuthentication.password.sendKeys(incorrectPassword);
+        formAuthentication.clickSubmitButton();
+        Assert.assertEquals(formAuthentication.getPageMessage(), passwordErrorMessage);
+
+        // submit blank password
+        formAuthentication.enterLoginInformation("", "");
+        formAuthentication.clickSubmitButton();
+        Assert.assertEquals(formAuthentication.getPageMessage(), usernameErrorMessage);
     }
 }
 
